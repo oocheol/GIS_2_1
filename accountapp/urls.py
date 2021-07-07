eae7823055803f4eaef1d67f608ad33fd177066c
oocheol <oocheol@naver.com>
2021-07-07 13:13:38 +0900

    main5

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -4,6 +4,6 @@ from accountapp.views import hello_world
 
 app_name = 'accountapp'
 
-urlpatterns = {
-    path('hello_world/', hello_world, name='hello_world')
-}
+urlpatterns = [
+    path('hello_world/', hello_world, name='hello_world'),
+]
