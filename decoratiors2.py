c848ba8da1737ef7e3296a4b35922d381b854b9c
oocheol <oocheol@naver.com>
2021-07-21 12:47:21 +0900

    07/21 4st

--- decoratiors2.py
+++ decoratiors2.py
@@ -1,5 +1,17 @@
-class User:
-    def __init__(self. auth):
-        self.is_authenticated = auth
+def decorator(func):
+    def decorated(h, w):
+        if h > 0 and w > 0:
+            func(h, w)
+        else:
+            print("Error")
+    return decorated
 
-    user = User(auth=False)
+@decorator
+def square(h, w):
+    print(h * w)
+
+def square2(h, w):
+    print(h * w)
+
+square(3,3)
+decorator(square2)(-3,3)
