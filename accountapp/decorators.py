741eae4b1e4c4b5ef4a662fbbf1988c0bb74dd97
oocheol <oocheol@naver.com>
2021-07-22 16:58:31 +0900

    07/22 1st

--- accountapp/decorators.py
+++ accountapp/decorators.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseForbidden
 
 def account_ownership_required(func):
     def decorated(request, *args, **kwargs):
-        target_user = User.objects.obj(pk=kwargs['pk'])
+        target_user = User.objects.get(pk=kwargs['pk'])
         if target_user == request.user:
             return func(request, *args, **kwargs)
         else :
