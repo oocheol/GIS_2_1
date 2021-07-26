d5c24365537243863a1f538558a40339e8cec1e3
oocheol <oocheol@naver.com>
2021-07-26 17:46:24 +0900

    07/26 3st

--- profileapp/models.py
+++ profileapp/models.py
@@ -12,5 +12,3 @@ class Profile(models.Model):
     image = models.ImageField(upload_to='profile/', null=True)
     nickname = models.CharField(max_length=30, unique=True, null=True)
     message = models.CharField(max_length=200, null=True)
-
-
